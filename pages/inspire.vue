1ad7a16adb433682565d14225e980d498f04a4f9
mlevi <mlevi@mdinonline.ru>
2020-03-25 13:47:01 +0300

    ci: Test pipeline commit

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -7,10 +7,10 @@
         class="mb-5"
       >
       <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
+        &#8220;Never trust the information from internet.&#8221;
         <footer>
           <small>
-            <em>&mdash;John Johnson</em>
+            <em>&mdash;Vladimir Lenin</em>
           </small>
         </footer>
       </blockquote>

f1682eb29066f10bdee289c53e725e99d54071de
mlevi <mlevi@mdinonline.ru>
2020-03-27 11:13:52 +0300

    feat: Vuex index module with Skills data

---
+++ store/index.js
@@ -0,0 +1,12 @@
+export const state = () => ({
+  primarySkills: {
+    Laravel: 'fab fa-laravel',
+    PHP: 'fab fa-php',
+    'Vue JS': 'fab fa-vuejs',
+    JavaScript: 'fab fa-js-square',
+    Docker: 'fab fa-docker',
+    Git: 'fab fa-git-alt',
+    HTML5: 'fab fa-html5',
+    CSS3: 'fab fa-css3-alt'
+  }
+})

a77d5a213dd68cef16e72ab85f45ac2fcb0aab98
mlevi <mlevi@mdinonline.ru>
2020-03-30 14:13:06 +0300

    chroe: Move initial data to vuex store

--- store/index.js
+++ store/index.js
@@ -1,4 +1,8 @@
 export const state = () => ({
+  ownerName: 'Mikhail Levi',
+  ownerSubtitle: 'Full Stack Web Developer',
+  ownerAvatarUrl: 'photos/mikhail-levi-bkk-2020.jpg',
+  ownerAvatarTitle: 'Mikhail Levi Bangkok 2020',
   primarySkills: {
     Laravel: 'fab fa-laravel',
     PHP: 'fab fa-php',
@@ -8,5 +12,22 @@ export const state = () => ({
     Git: 'fab fa-git-alt',
     HTML5: 'fab fa-html5',
     CSS3: 'fab fa-css3-alt'
-  }
+  },
+  contacts: [
+    {
+      name: '@mikhail_levi',
+      icon: 'fab fa-telegram-plane',
+      link: 'https://t.me/mikhail_levi'
+    },
+    {
+      name: 'LinkedIn Profile',
+      icon: 'fab fa-linkedin',
+      link: 'https://ru.linkedin.com/in/mikhail-levi-913b777a'
+    },
+    {
+      name: '@mindonline',
+      icon: 'fab fa-github',
+      link: 'https://github.com/mindonline'
+    }
+  ]
 })

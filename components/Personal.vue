99bd976cf0bd75108428251dd288618c20d26f60
mlevi <mlevi@mdinonline.ru>
2020-10-09 22:14:43 +0300

    feature: personal block

---
+++ components/Personal.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-card>
+    <v-card-text>
+      {{ personal }}
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+
+import moment from 'moment'
+
+export default {
+  name: 'Personal',
+  props: {
+    birthday: {
+      type: Date,
+      required: true
+    }
+  },
+  computed: {
+    personal () {
+      return this.$store.state.personal.replace('%years%', this.years)
+    },
+    years () {
+      return moment().diff(moment(this.birthday), 'years')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-img src="/assets/bg-index.jpg" height="100%">
+  <v-img :src="backgroundImage" height="100%">
     <v-container>
       <v-row>
         <v-col cols="12" md="6" offset-md="3">
@@ -14,9 +14,15 @@
       <v-row wrap justify-center align-start>
         <v-col cols="12" md="6" offset-md="3">
           <div class="title mt-4 mb-4">
+            About
+          </div>
+          <personal :birthday="$store.state.birthday"></personal>
+        </v-col>
+        <v-col cols="12" md="6" offset-md="3">
+          <div class="title mt-4 mb-4">
             Contacts
           </div>
-          <contact-list :value="$store.state.contacts" />
+          <contact-list :value="$store.state.contacts"/>
         </v-col>
         <v-col cols="12" md="6" offset-md="3">
           <div class="title mt-4 mb-4">
@@ -24,7 +30,7 @@
           </div>
           <v-card>
             <v-card-text>
-              <skills-badges :value="$store.state.primarySkills" />
+              <skills-badges :value="$store.state.primarySkills"/>
             </v-card-text>
           </v-card>
         </v-col>
@@ -47,23 +53,39 @@
 </template>
 
 <script>
-  import SkillsBadges from '../components/SkillsBadges'
-  import ContactList from '../components/ContactList'
-  import ProfileAvatar from '../components/ProfileAvatar'
-  import EfficiencyChart from '../components/EfficiencyChart'
+import SkillsBadges from '../components/SkillsBadges'
+import ContactList from '../components/ContactList'
+import ProfileAvatar from '../components/ProfileAvatar'
+import EfficiencyChart from '../components/EfficiencyChart'
+import Personal from '@/components/Personal'
 
-  export default {
-    components: {
-      EfficiencyChart,
-      ProfileAvatar,
-      ContactList,
-      SkillsBadges
-    },
+export default {
+  components: {
+    Personal,
+    EfficiencyChart,
+    ProfileAvatar,
+    ContactList,
+    SkillsBadges
+  },
 
-    head () {
-      return {
-        title: 'About me'
+  computed: {
+    backgroundImage () {
+      switch (this.$vuetify.breakpoint.name) {
+      case 'xs':
+      case 'sm':
+      case 'md':
+        return '/assets/bg-index-mobile.jpg'
+      default:
+        return '/assets/bg-index.jpg'
       }
+
+    }
+  },
+
+  head () {
+    return {
+      title: 'About me'
     }
   }
+}
 </script>
